74235e2057c2b7548454e08ceaed52cfc7818f6f
Sam Mulhearn <sam.mulhearn@gmail.com>
2019-01-02 17:47:15 +0000

    Remove '&' from String

--- Modules/Publish-GPOtoAD.psm1
+++ Modules/Publish-GPOtoAD.psm1
@@ -36,12 +36,12 @@ Function Publish-GPOtoAD {
             Write-log -Path $CurrentLogFile -Message "Imported AD/GPO Modules"
             if ($VDA -ne $null -or $VDA -ne "") {
                 if (Test-Path -Path "\\$env:USERDNSDOMAIN\SYSVOL\$env:USERDNSDOMAIN\Policies\PolicyDefinitions" -PathType Container) {
-                    Write-Log -Path $CurrentLogFIle -Message "Copying ADMX/ADML files for BIS-F & FSLogix to the Central Store"
+                    Write-Log -Path $CurrentLogFIle -Message "Copying ADMX/ADML files for BIS-F and FSLogix to the Central Store"
                     Copy-Item -Path "\\$VDA\\C$\Program Files (x86)\Base Image Script Framework (BIS-F)\ADMX\BaseImageScriptFramework.admx" -Destination "\\$env:USERDNSDOMAIN\SYSVOL\$env:USERDNSDOMAIN\Policies\PolicyDefinitions" -Force
                     Copy-Item -Path "\\$VDA\\C$\Program Files (x86)\Base Image Script Framework (BIS-F)\ADMX\en-US\BaseImageScriptFramework.adml" -Destination "\\$env:USERDNSDOMAIN\SYSVOL\$env:USERDNSDOMAIN\Policies\PolicyDefinitions\en-US" -Force
                     Copy-Item -Path "C:\Cetus\Software\FSLogix\fslogix.admx" -Destination "\\$env:USERDNSDOMAIN\SYSVOL\$env:USERDNSDOMAIN\Policies\PolicyDefinitions" -Force
                     Copy-Item -Path "C:\Cetus\Software\FSLogix\fslogix.adml" -Destination "\\$env:USERDNSDOMAIN\SYSVOL\$env:USERDNSDOMAIN\Policies\PolicyDefinitions\en-US" -Force
-                    Write-Log -Path $CurrentLogFIle -Message "Copied ADMX/ADML files for BIS-F & FSLogix to the Central Store"
+                    Write-Log -Path $CurrentLogFIle -Message "Copied ADMX/ADML files for BIS-F and FSLogix to the Central Store"
                 }
             }
         }
