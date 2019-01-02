4ca637be71c581f989acd23a3b1ae728f7e1679d
Sam Mulhearn <sam.mulhearn@gmail.com>
2019-01-02 17:26:14 +0000

    Added Concatenation script
    Added Concatenation

--- Modules/Concatenate.bat
+++ Modules/Concatenate.bat
@@ -1,1 +1,4 @@
-type *.psm1 > concatenated.psm1
+@echo off
+@echo new-module -name CitrixBuild -scriptblock { > web.txt
+type *.psm1 >> web.txt
+@echo export-modulemember -Function Copy-FromISO,Copy-FromZip,Publish-GPOtoAD,Copy-RoboMirror,Get-OrdinalNumber,Read-BooleanQuestion,Select-Products,Set-Parameters,Initialize-Machine,Install-CitrixProduct,Install-Software,Install-VDA,Install-WEM,Install-WinRolesAndFeatures,Test-JSONFile,Write-Log } >> web.txt
